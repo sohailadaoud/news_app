33bff0a83e4111ab481d26bb5e98eb5cd6d9cb90
Sohaila Daoud <63005434+sohailadaoud@users.noreply.github.com>
2023-10-13 23:01:36 +0300

    settings UI only.

--- lib/settings/settings_tab.dart
+++ lib/settings/settings_tab.dart
@@ -1,11 +1,37 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:news_app/myTheme.dart';
 
 class SettingsTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: MyTheme.yellowColor,
+      margin: EdgeInsets.all(20),
+      child: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+        SizedBox(
+          height: 40,
+        ),
+        Text(
+          'Language',
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        InkWell(
+          onTap: () {},
+          child: Container(
+            padding: EdgeInsets.all(8),
+            decoration: BoxDecoration(color: Colors.white),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('language'),
+                Icon(Icons.arrow_drop_down, color: MyTheme.primaryLightColor)
+              ],
+            ),
+          ),
+        )
+      ]),
     );
   }
 }
