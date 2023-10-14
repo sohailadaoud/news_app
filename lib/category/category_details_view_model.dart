f88795c2625f5905c163944dae3d07cc0bcc3cdb
Sohaila Daoud <63005434+sohailadaoud@users.noreply.github.com>
2023-10-14 22:32:47 +0300

    mvvm part1

---
+++ lib/category/category_details_view_model.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/model/SourceResponse.dart';
+
+class CategoryDetailsViewModel extends ChangeNotifier {
+  List<Source>? sourcesList;
+
+  void getSource() {}
+}
